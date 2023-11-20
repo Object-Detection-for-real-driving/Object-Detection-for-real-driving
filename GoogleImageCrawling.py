2419ab35cdb927b0969c6fb0951de97efa89989d
JSeongH <sungho9807@gmail.com>
2023-11-20 20:22:24 +0900

    update

--- GoogleImageCrawling.py
+++ GoogleImageCrawling.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from urllib.request import urlretrieve
 import time
@@ -36,23 +37,43 @@ class OpenPage:
 
     def find_image(self):
         images = driver.find_elements(By.CLASS_NAME, "rg_i.Q4LuWd")
-        limit = int(input(f"{len(images)}개 중 이미지 개수 : "))  
-        
-        if len(images) >= limit and len(images) > 0:
-            crawling_images(self.keyword, images, limit)
-        else:
-            print(f"{len(images)}개 밖에 이미지가 없습니다.")
-            limit = len(images)
+        limit = input(f"{len(images)}개 중 이미지 개수 : ")
 
-            crawling_images(self.keyword, images, limit)
-        
+        try:
+            limit = int(limit)
+
+            if len(images) >= limit and len(images) > 0:
+                crawling_images(self.keyword, images, limit)
+
+            else:
+                images = scrollDown(images, limit)
+                crawling_images(self.keyword, images, limit)
+
+        except:
+            if limit == '재검색':
+                self.searching_image()
+            
+            print("정수로 입력해주세요.")
+            self.find_image()
+            
 
     def closePage(self):
         driver.close()
 
 
+def scrollDown(images, limit):
+    while limit > len(images):
+        action = ActionChains(driver)
+        action.move_to_element(images[-1]).perform()
+
+        images = driver.find_elements(By.CLASS_NAME, "rg_i.Q4LuWd")
+        print(f"이미지 {len(images)}개 찾음")
+
+    return images
+
+
 def crawling_images(keyword, images, limit):
-    current_path = "C:\workspace\VScode-workspace\Object_Detection\data"
+    current_path = "./data"
     folder = keyword
     image_folder = os.path.join(current_path, folder)
     name = input("저장할 이름을 적어주세요 : ")
@@ -71,7 +92,7 @@ def crawling_images(keyword, images, limit):
                 print(f'{idx}번째 이미지 오류')
                 wrong.append(image)
 
-    print(f"{len(images) - len(wrong)}장 저장되었습니다.")
+    print(f"{limit - len(wrong)}장 저장되었습니다.")
 
 
 def main():
